98c6e96fde4c67c5f26496a032e26979b05136c5
ayasamind <masayaliketennis@yahoo.co.jp>
2019-03-23 15:51:55 +0900

    管理者の認証機能を作成

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,12 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('admin')->group(function () {
+    Route::get("/login", "Admin\Auth\LoginController@showLoginForm")->name('admin.auth.login_form');
+    Route::post("/login", "Admin\Auth\LoginController@login")->name('admin.auth.login');;
+    Route::post("/logout", "Admin\Auth\LoginController@logout");
+    Route::get("/register", "Admin\Auth\RegisterController@showRegistrationForm")->name('admin.auth.register_form');
+    Route::post("/register", "Admin\Auth\RegisterController@register")->name('admin.auth.register');
+    Route::get("/home", "Admin\HomeController@index")->name('admin.home');
+});
